bf2b8d21d596a2cb08e855985609104dfcb7dfcf
James Langridge <james.d.langridge@gmail.com>
2021-02-23 20:18:43 +0100

    refactor directory structure

---
+++ models/file.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const fileSchema = mongoose.Schema(
+  {
+    word: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    syllable: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    image_url: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const File = mongoose.model('File', fileSchema);
+
+module.exports = File;
---
+++ server.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require('path');
+
+const users = require("./routes/api/users");
+const fileRoute = require('./routes/file');
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static(__dirname));
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+// what is the first string arg here?
+app.use("/api/users", users);
+app.use("/file", fileRoute);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy there
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
