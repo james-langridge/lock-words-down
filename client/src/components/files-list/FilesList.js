4addbb9daf222ee316ca7d69531e654fd585dedc
James Langridge <james.d.langridge@gmail.com>
2021-02-26 19:38:26 +0100

    improve card buttons in gallery view, improve alert if no words

--- client/src/components/files-list/FilesList.js
+++ client/src/components/files-list/FilesList.js
@@ -7,6 +7,8 @@ import axios from 'axios';
 import { Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Alert from 'react-bootstrap/Alert';
 import WordCard from './WordCard';
 
 const FilesList = (props) => {
@@ -70,24 +72,33 @@ const FilesList = (props) => {
     window.location.href = "./login";
   }
 
+  const saveSelection = () => {
+    console.log('selectedWords', selectedWords);
+  }
+
 return (
   <div>
     <Button variant="primary" as={Link} to="/upload">Add word</Button>
     <Button className={`mx-2 ${selectedWords.length ? "" : "disabled"}`} variant="success" as={Link} to="/game">Play game</Button>
-    <Button variant="danger" onClick={() => logOut()}>Log out</Button>
+    <Button className="mx-2" variant="danger" onClick={() => logOut()}>Log out</Button>
     <div className="photos-list">
-    {filesList.length > 0 &&
+    {filesList.length > 0 ?
       filesList.map((word) =>
-      <Card className="photo" key={word._id} id={word._id} style={{ width: '18rem' }}>
+      <Card className="photo box-shadow" key={word._id} id={word._id} style={{ width: '18rem' }}>
         <Card.Img variant="top" src={word.image_url} onClick={() => handleClick(word)} />
         <Card.Body>
           <Card.Title>{word.word}</Card.Title>
           <Card.Text>{word.syllable}</Card.Text>
-          <Card.Link as={Link} to={'/edit?id=' + word._id}>Edit</Card.Link>
-          <Card.Link href="#" onClick={() => handleDelete(word._id)}>Delete</Card.Link>
+          <div className="d-flex justify-content-between align-items-center">
+            <ButtonGroup size="sm">
+              <Button variant="outline-secondary" onClick={() => handleClick(word)}>Select</Button>
+              <Button variant="outline-secondary" as={Link} to={'/edit?id=' + word._id}>Edit</Button>
+              <Button variant="outline-secondary" onClick={() => handleDelete(word._id)}>Delete</Button>
+            </ButtonGroup>
+          </div>
         </Card.Body>
       </Card>
-      )
+    ) : <Alert variant="primary"><Alert.Link as={Link} to="/upload">Add some words!</Alert.Link></Alert>
     }
     </div>
   </div>
