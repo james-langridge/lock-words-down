eb94c8b70e160469eb8fbb59457c46143e150b4c
James Langridge <james.d.langridge@gmail.com>
2021-02-27 10:38:42 +0100

    increase syllable border-radius

--- client/src/components/game/Syllable.js
+++ client/src/components/game/Syllable.js
@@ -4,7 +4,7 @@ import { Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
   border: 1px solid lightgrey;
-  border-radius: 2px;
+  border-radius: 25px;
   padding: 8px;
   margin: 8px;
   background-color: ${props => (props.isDragging ? 'lightgreen' : '#F6FCE6')};
